804a9274e24217b8a3a5cfbd65914d4f15c4ac21
Kartang12 <vasiltsow.by@gmail.com>
2022-09-24 18:10:09 +0300

    ReviewServiceComplete

---
+++ ReviewApi/Contracts/Requests/CreateReviewRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace ReviewApi.Contracts.Requests
+{
+    public class CreateReviewRequest
+    {
+        public Guid HotelId { get; set; }
+        public Guid UserId { get; set; }
+        public string HotelName { get; set; }
+        public string UserName { get; set; }
+        public int Score { get; set; }
+        public string Feedback { get; set; }
+    }
+}
---
+++ ReviewApi/Services/ReviewService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ReviewApi.Contracts.Requests;
+using ReviewApi.DbContext;
+using ReviewApi.Domain;
+
+namespace ReviewApi.Services
+{
+    public class ReviewService
+    {
+        readonly ReviewsContext _context;
+        private readonly IMapper _mapper;
+
+        public ReviewService(ReviewsContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        public async Task<IEnumerable<Review>> GetByIdsAsync(IEnumerable<Guid> ids)
+        {
+            return await _context.Reviews.Where(x => ids.Contains(x.Id)).ToListAsync();
+        }
+
+        public async Task<Review> GetByIdAsync(Guid id)
+        {
+            return await _context.Reviews.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Review> CreateAsync(CreateReviewRequest request)
+        {
+            var newReview = _mapper.Map<Review>(request);
+            var result = await _context.Reviews.AddAsync(newReview);
+            return result.Entity;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var toDelete = await _context.Reviews.FirstOrDefaultAsync(x => x.Id == id);
+            _context.Reviews.Remove(toDelete);
+        }
+
+        public async Task<Review> UpdateAsync(Review toUpdate)
+        {
+            var result = _context.Reviews.Update(toUpdate);
+            return result.Entity;
+        }
+    }
+}
